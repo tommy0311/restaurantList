a74f8af52dd2fbb2a18dc751f95105cc4a6c53a9
litangchen <chen.li.tang@gmail.com>
2022-05-27 16:53:04 +0800

    feat: add sort drop menu in the index page

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -3,13 +3,34 @@ const router = express.Router()
 
 const RestaurantList = require('../../models/restaurantsList')
 
+const sortStringMap = {
+	name:      "餐廳名稱：A → Z",
+  name_desc: "餐廳名稱：Z → A",
+  category:  "類別",
+  location:  "地區",
+}
+
+const sortSqlMap = {
+	name:      { name: 'asc' },
+  name_desc: { name_desc: 'desc' },
+  category:  { category: 'asc' },
+  location:  { location: 'asc' },
+}
+
 router.get("/", (req, res) => {
 	const keyword = req.query.keyword
+  let sortQuery = req.query.sortQuery
+  if(!sortQuery){
+    sortQuery = 'name'
+  }
+	//console.log("search:", req.query)
+	
 	return RestaurantList.find( {$or: [
 	  {"name": {"$regex": keyword, "$options": "i"}},
 	  {"category": {"$regex": keyword, "$options": "i"}}]})
 	  .lean()
-	  .then(restaurants => res.render("index", { restaurants, keyword }))
+    .sort(sortSqlMap[sortQuery])
+	  .then(restaurants => res.render("index", { restaurants, keyword, sortQuery, sortString:sortStringMap[sortQuery] }))
 	  .catch(error => {
 	    console.log(error)
 	    res.redirect('/')
