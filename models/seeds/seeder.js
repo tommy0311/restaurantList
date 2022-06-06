3b911d84b5d7a9f6b2e8dd59032f774d9aa16e5e
litangchen <chen.li.tang@gmail.com>
2022-06-06 23:02:57 +0800

    fix: seed exit condinition

--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -21,48 +21,45 @@ const SEED_USER2 = {
   idMap: [ 4, 5, 6 ]
 }
 
+
 const SEED_USERS = [ SEED_USER1, SEED_USER2 ]
 
 db.once('open', () => {
   let finish = []
-  new Promise((resolve, _reject) => {
-    for (const [index, seed] of SEED_USERS.entries()) {
-      bcrypt
-      .genSalt(10)
-      .then(salt => {
-        return bcrypt.hash(seed.password, salt)
-      })
-      .then(hash => { 
-            return User.create({
-              name: seed.name,
-              email: seed.email,
-              password: hash})
-          }
-      )
-      .then(user => {
-        const userId = user._id
-        return Promise.all(Array.from(
-          { length: seed.idMap.length },
-          (_, i) => {
-            const result = restaurants.filter(obj => Number(obj.id) === seed.idMap[i])
-            if( result.length != 1) {
-              //resolve()
-            }
-            result[0].userId = userId
-            return RestaurantList.create(result[0])
-          }
-        ))
-      })
-      .then(() => {
-        finish.push( index )
-        if (finish.length === SEED_USERS.length) {
-          resolve()
+  for (const [index, seed] of SEED_USERS.entries()) {
+    bcrypt
+    .genSalt(10)
+    .then(salt => {
+      return bcrypt.hash(seed.password, salt)
+    })
+    .then(hash => { 
+          return User.create({
+            name: seed.name,
+            email: seed.email,
+            password: hash})
         }
-      })
-      .catch( error=> console.log(error) )
-    }
-  })
-  .then(() => {
-    process.exit()
-  })
+    )
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: seed.idMap.length },
+        (_, i) => {
+          const result = restaurants.filter(obj => Number(obj.id) === seed.idMap[i])
+          if( result.length != 1) {
+            resolve()
+          }
+          result[0].userId = userId
+          return RestaurantList.create(result[0])
+        }
+      ))
+    })
+    .then(() => {
+      finish.push( index )
+      if (finish.length === SEED_USERS.length) {
+        console.log('done')
+        process.exit()
+      }
+    })
+    .catch( error=> console.log(error) )
+  }
 })
