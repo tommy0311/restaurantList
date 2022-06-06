8c95a20ebf0e8aea5962a5d37ff2788633041072
litangchen <chen.li.tang@gmail.com>
2022-06-06 20:21:50 +0800

    feat: add seeder

--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -1,20 +1,62 @@
-const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const RestaurantList = require('../restaurantsList')
 const { results: restaurants } = require('../../restaurant.json')
+const User = require('../user')
+const db = require('../../config/mongoose')
 
-mongoose.connect(process.env.RESTAURANT_MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const SEED_USER1 = {
+  name: 'user1',
+  email: 'user1@example.com',
+  password: '12345678',
+  idMap: [ 1, 2, 3 ]
+}
 
-const db = mongoose.connection
+const SEED_USER2 = {
+  name: 'user2',
+  email: 'user2@example.com',
+  password: '12345678',
+  idMap: [ 4, 5, 6 ]
+}
 
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const SEED_USERS = [ SEED_USER1, SEED_USER2 ]
 
 db.once('open', () => {
-  console.log('mongodb connected!');
-
-  for (let i = 0; i < restaurants.length ; i++) {
-    RestaurantList.create(restaurants[i])
+  for (const [index, seed] of SEED_USERS.entries()) {
+    bcrypt
+    .genSalt(10)
+    .then(salt => {
+      return bcrypt.hash(seed.password, salt)
+    })
+    .then(hash => { 
+          return User.create({
+            name: seed.name,
+            email: seed.email,
+            password: hash})
+        }
+    )
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: seed.idMap.length },
+        (_, i) => {
+          const result = restaurants.filter(obj => Number(obj.id) === seed.idMap[i])
+          if( result.length != 1) {
+            resolve()
+          }
+          result[0].userId = userId
+          return RestaurantList.create(result[0])
+        }
+      ))
+    })
+    .then(() => {
+      if (index >= SEED_USERS.length - 1) {
+        console.log('done')
+        process.exit()
+      }
+    })
+    .catch( error=> console.log(error) )
   }
-  console.log('done')
 })
